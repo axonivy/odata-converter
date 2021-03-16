8891983f3150203e372e4b993730b0edbee2b57c
Alexander Suter <alexander.suter@axonivy.com>
2021-03-16 13:49:58 +0100

    Harden generator script

--- scripts/convert-job.sh
+++ scripts/convert-job.sh
@@ -13,8 +13,8 @@ if grep -q DataServiceVersion= "$file"; then
   name=$(basename $file)
   nameNoExtension=$(echo "$name" | cut -f 1 -d '.')
 
-  docker run -u 1000 -v /opt/data:/tmp/work axonivy/build-container:odata-converter-2 -p /tmp/work/input/$name -t /tmp/work/output/tmp$nameNoExtension.json &>> $log
-  mv /opt/data/output/tmp$nameNoExtension.json /opt/data/output/nameNoExtension.json
+  docker run -u 1000 -v /opt/data:/tmp/work axonivy/build-container:odata-converter-2 -p /tmp/work/input/$name -t /tmp/work/output/$nameNoExtension.json.tmp &>> $log
+  mv /opt/data/output/$nameNoExtension.json.tmp /opt/data/output/$nameNoExtension.json
 
 else
   echo OData version 3/4 detected >> $log
@@ -22,6 +22,6 @@ else
   name=$(basename $file)
   nameNoExtension=$(echo "$name" | cut -f 1 -d '.')
 
-  docker run -u 1000 -v /opt/data:/tmp/work axonivy/build-container:odata-converter-4 -p /tmp/work/input/$name -t /tmp/work/output/tmp$nameNoExtension.json &>> $log
-  mv /opt/data/output/tmp$nameNoExtension.json /opt/data/output/nameNoExtension.json
+  docker run -u 1000 -v /opt/data:/tmp/work axonivy/build-container:odata-converter-4 -p /tmp/work/input/$name -t /tmp/work/output/$nameNoExtension.json.tmp &>> $log
+  mv /opt/data/output/$nameNoExtension.json.tmp /opt/data/output/$nameNoExtension.json
 fi
