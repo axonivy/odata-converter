f9942679779909b29e5dbcecf2275fee46e83401
Alexander Suter <alexander.suter@axonivy.com>
2021-03-17 15:19:51 +0100

    Small improvements

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
-# OData to Open API converter
+# OData to OpenAPI converter
+
+Website to convert OData specification to OpenAPI. The conversion will be done
+with the help of `axonivy/builder-containers:odata-converter`. Because the
+conversion can be a long running process this will be processed asynchron with a
+directory watcher.
 
 ## Install
 
-install docker and docker-compose
+1) install docker and docker-compose
 
-    git clone https://github.com/axonivy/odata-converter
-    docker-compose up -d
-    docker-compose exec web composer install
+2) set file permission for data directory
 
-set file permission for data directory
+        mkdir -p /opt/data/input
+        mkdir -p /opt/data/output
+        sudo chown -R 1000:1000 /opt/data 
 
-    sudo apt-get install incron
-    echo "ubuntu" | sudo tee -a /etc/incron.allow
-    incrontab -e
-    mkdir /opt/data/output
-    /opt/data/input IN_CREATE /opt/odata-converter/scripts/convert-job.sh $@ $#
+3) start docker compose
+
+        git clone https://github.com/axonivy/odata-converter
+        docker-compose up -d
+        docker-compose exec web composer install
+
+4) install incron to watch for new files
+        
+        sudo apt-get install incron
+        echo "root" | sudo tee -a /etc/incron.allow
+        sudo incrontab -e
+        /opt/data/input IN_CREATE /opt/odata-converter/scripts/convert-job.sh $@ $#
--- scripts/convert-job.sh
+++ scripts/convert-job.sh
@@ -10,26 +10,21 @@ if [ "$extension" != "xml" ]; then
   exit
 fi
 
-#m:DataServiceVersion="2.0"
+mkdir -p /opt/data/output
+chown 1000:1000 /opt/data/output
 
 log=/opt/data/output/$2.log
 echo Start converting $2 > $log
 
+name=$(basename $file)
+nameNoExtension=$(echo "$name" | cut -f 1 -d '.')
+
 if grep -q DataServiceVersion= "$file"; then
   echo OData version 2 detected >> $log
-
-  name=$(basename $file)
-  nameNoExtension=$(echo "$name" | cut -f 1 -d '.')
-
-  docker run -u 1000 -v /opt/data:/tmp/work axonivy/build-container:odata-converter-2 -p /tmp/work/input/$name -t /tmp/work/output/$nameNoExtension.json.tmp &>> $log
-  mv /opt/data/output/$nameNoExtension.json.tmp /opt/data/output/$nameNoExtension.json
-
+  docker run -u 1000 -v /opt/data:/tmp/work axonivy/build-container:odata-converter-2 -p /tmp/work/input/$name -t /tmp/work/output/$nameNoExtension.json &>> $log
 else
   echo OData version 3/4 detected >> $log
-
-  name=$(basename $file)
-  nameNoExtension=$(echo "$name" | cut -f 1 -d '.')
-
-  docker run -u 1000 -v /opt/data:/tmp/work axonivy/build-container:odata-converter-4 -p /tmp/work/input/$name -t /tmp/work/output/$nameNoExtension.json.tmp &>> $log
-  mv /opt/data/output/$nameNoExtension.json.tmp /opt/data/output/$nameNoExtension.json
+  docker run -u 1000 -v /opt/data:/tmp/work axonivy/build-container:odata-converter-4 -p /tmp/work/input/$name -t /tmp/work/output/$nameNoExtension.json &>> $log
 fi
+
+echo Conversion finished >> $log
